011f25b44765062a5c0df6f7ce2e2bd5ae50e421
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-03-09 11:24:54 +0200

    444

---
+++ bento-grid-main/task1.js
@@ -0,0 +1,9 @@
+// const change = document.querySelector(".imgcreat")
+
+// console.log(change.width);
+
+// setTimeout(() => {
+//     change.style.width = "700px";
+// }, 7000);
+
+// console.log(change.width);
